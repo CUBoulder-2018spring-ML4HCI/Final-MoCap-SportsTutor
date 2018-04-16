28ec8151c37def72b595ef9b722b0af77620eec1
Jonathan Fermin <jofe4577@colorado.edu>
2018-04-16 14:47:51 -0600

    edited ui

--- Python/SportsTutor/main.py
+++ Python/SportsTutor/main.py
@@ -11,29 +11,24 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self.returning_button.clicked.connect(self.returningUserClicked)
-        self.new_button.clicked.connect(self.newUserClicked)
-        self.actionNew_Recording.triggered.connect(self.newRecordingClicked)
-        """
-        self.continue_button.clicked.connect(self.continueClicked)
-        self.restart_button.clicked.connect(self.returnClicked)
-<<<<<<< HEAD
-        self.stackedWidget.setCurrentIndex(0)
+        # self.returning_button.clicked.connect(self.returningUserClicked)
+        # self.new_button.clicked.connect(self.newUserClicked)
+        # self.actionNew_Recording.triggered.connect(self.newRecordingClicked)
+        self.continue_button.clicked.connect(self.nextPage)
+        self.BackButton.clicked.connect(self.prevPage)
+        self.BackButton_2.clicked.connect(self.prevPage)
+        self.BackButton_3.clicked.connect(self.prevPage)
 
-    def startClicked(self):
-        self.stackedWidget.setCurrentIndex(1)
-    def continueClicked(self):
-=======
-        """
-        
+    def nextPage(self):
+        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex()+1)
+    def prevPage(self):
+        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex()-1)
     def newRecordingClicked(self):
         return 1
     def returningUserClicked(self):
         self.stackedWidget.setCurrentIndex(2)
     def newUserClicked(self):
         self.stackedWidget.setCurrentIndex(1)
-    def returnClicked(self):
-        self.stackedWidget.setCurrentIndex(0)
  
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
